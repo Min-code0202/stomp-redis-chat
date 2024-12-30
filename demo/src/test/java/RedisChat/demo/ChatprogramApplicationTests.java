767374d81a9d3a05b34011fa66f8ecd103ebbb1b
JM <jjm001115@gmail.com>
2024-12-30 17:14:25 +0900

    first commit

---
+++ demo/src/test/java/RedisChat/demo/ChatprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package RedisChat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
